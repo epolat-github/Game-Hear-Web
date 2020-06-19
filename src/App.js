49565275a05b2ea1addd256584a73ad77998f845
epolat <etuna1999@gmail.com>
2020-06-19 10:58:23 +0300

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from "react";
-import Header from "./components/Header";
-import About from "./components/About";
-import Links from './components/Links'
+import { Switch, Route } from "react-router-dom";
+
+import Main from "./components/Main";
+import Games from "./components/Games";
+import EmailSub from "./components/EmailSub";
+
 import "./App.css";
 
 const App = () => {
     return (
         <div className="App">
-            <Header />
-            <About />
-            <Links />
+            <Switch>
+                <Route exact path="/">
+                    <Main />
+                </Route>
+                <Route exact path="/games">
+                    <Games />
+                </Route>
+                <Route exact path="/email">
+                    <EmailSub />
+                </Route>
+            </Switch>
         </div>
     );
 };
