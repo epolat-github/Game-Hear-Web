d83a276af3aef70fd2ded95a5459872dfcc09d4c
epolat-github <etuna1999@gmail.com>
2020-06-21 21:42:56 +0300

    add click handler to Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Button = ({ style, text, link, targetRef }) => {
+const Button = ({ style, text, link, targetRef, onClickHandler }) => {
     return (
         <div className="button-container">
             {targetRef === "inside" ? (
                 <Link to={link}>
-                    <button className={style}>{text}</button>
+                    <button className={style} onClick={onClickHandler}>
+                        {text}
+                    </button>
                 </Link>
             ) : (
                 <a href={link} target="_blank">
-                    <button className={style}>{text}</button>
+                    <button className={style} onClick={onClickHandler}>
+                        {text}
+                    </button>
                 </a>
             )}
         </div>
