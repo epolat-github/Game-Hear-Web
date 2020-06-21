81810e434d13cb463db5a0c1043e2a750b9fc010
epolat-github <etuna1999@gmail.com>
2020-06-21 21:41:32 +0300

    create games list component

---
+++ src/components/GamesNavbar.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import Button from "./Button";
+
+const GamesNavbar = ({ games }) => {
+    const [active, setActive] = useState(Math.floor(games.length / 2));
+
+    const onClickHandler = (index) => {
+        setActive(index);
+    };
+
+    return (
+        <nav id="games-navbar-container">
+            {games.map((gameName, index) => (
+                <Button
+                    style={
+                        index === active ? "secondary-button active" : "primary-button"
+                    }
+                    text={gameName}
+                    link={`#${gameName}`}
+                    targetRef="inside"
+                    onClickHandler={() => onClickHandler(index)}
+                ></Button>
+            ))}
+        </nav>
+    );
+};
+
+export default GamesNavbar;
