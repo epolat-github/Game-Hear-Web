4586c5fcc72d663f6705b33abecdc0e2083990ba
epolat-github <etuna1999@gmail.com>
2020-06-22 18:21:48 +0300

    add games backend communication

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,16 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import SingleNewsCard from "./SingleNewsCard";
 import Header from "./Header";
 import GamesNavbar from "./GamesNavbar";
 
 const Games = () => {
+    const [newsList, setNewsList] = useState([]);
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(
+                    "https://game-hear-backend.herokuapp.com/api/gta5/news"
+                );
+                if (!response.ok) {
+                    throw new Error("Network response is not OK.");
+                }
+                const data = await response.json();
+                setIsLoaded(true);
+                setNewsList(data);
+            } catch (err) {
+                console.log(err);
+                setIsLoaded(true);
+                setError(err);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div className="loading">Loading...</div>;
+    }
+
     return (
         <section className="games-container">
             <Header />
             <GamesNavbar games={["Valorant", "GTA5", "LoL"]} />
             <section className="cards-container">
-                <SingleNewsCard
+                {newsList.map((news) => (
+                    <SingleNewsCard
+                        date={new Date(news.newDate)}
+                        header={news.newHeader}
+                        image={{
+                            url: news.images[0],
+                            dimensions: {
+                                width: undefined,
+                                height: undefined,
+                            },
+                        }}
+                        news={news.news}
+                    />
+                ))}
+                {/* <SingleNewsCard
                     date={new Date()}
                     header="Euu eiusmod qui dolore do duis ipsum cillum officia sit ut exercitation enim voluptate reprehenderit."
                     image={{
@@ -47,7 +93,7 @@ const Games = () => {
                         "Ad duis nisi Lorem non qui.",
                         "Dolor non veniam reprehenderit est minim.",
                     ]}
-                />
+                /> */}
             </section>
         </section>
     );
