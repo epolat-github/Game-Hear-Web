ba4cf769b5153de5708b1d47530067cb9212e069
epolat-github <etuna1999@gmail.com>
2020-06-22 21:49:34 +0300

    add @babel/polyfill for async functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const ErrorOverlayPlugin = require("error-overlay-webpack-plugin");
+require("@babel/polyfill");
 const path = require("path");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: ["@babel/polyfill", "./src/index.js"],
     output: {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
